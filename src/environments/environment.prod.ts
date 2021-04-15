b7191e9c1588ba1d606ba6c977b6af551ccf4be9
Michael Murphy <82483919+webdevmurphy@users.noreply.github.com>
2021-04-15 17:38:32 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyAPWAV0H1P4petxGIt4EJM2_DRDCxEsiy0",
-    authDomain: "nebularproj2.firebaseapp.com",
-    databaseURL: "https://nebularproj2.firebaseio.com",
-    projectId: "nebularproj2",
-    storageBucket: "nebularproj2.appspot.com",
-    messagingSenderId: "759284366083",
-    appId: "1:759284366083:web:3602dfd31dd893b295c736",
-    measurementId: "G-1KD9QBXPWN"
+    
  }
 };
